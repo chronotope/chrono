John-Nagle/rust-chrono

initial implementation of `Offset` redesign (#11).
- We have splitted `Offset` into `Offset` and `OffsetState` (name
  changes in consideration). The former is used to construct and convert
  local or UTC date, and the latter is used to store the UTC offset
  inside constructed values. Some offsets are their own states as well.

- This uses lots of associated types which implementation is still in
  flux. Currently it crashes with debuginfo enabled. We've temporarily
  disabled debuginfo from `Cargo.toml`.

- This technically allows a conversion to the local time, but not yet
  tested.

src/format.rs
@@ -10,7 +10,7 @@ use std::fmt;
 
 use {Datelike, Timelike};
 use duration::Duration;
-use offset::Offset;
+use offset::OffsetState;
 use naive::date::NaiveDate;
 use naive::time::NaiveTime;
 
@@ -198,11 +198,11 @@ impl<'a> DelayedFormat<'a> {
         DelayedFormat { date: date, time: time, off: None, fmt: fmt }
     }
 
-    /// Makes a new `DelayedFormat` value out of local date and time and UTC offset.
+    /// Makes a new `DelayedFormat` value out of local date and time with offset state.
     pub fn new_with_offset<Off>(date: Option<NaiveDate>, time: Option<NaiveTime>,
-                                offset: &Off, fmt: &'a str) -> DelayedFormat<'a>
-            where Off: Offset + fmt::String {
-        let name_and_diff = (offset.to_string(), offset.local_minus_utc());
+                                state: &Off, fmt: &'a str) -> DelayedFormat<'a>
+            where Off: OffsetState + fmt::String {
+        let name_and_diff = (state.to_string(), state.local_minus_utc());
         DelayedFormat { date: date, time: time, off: Some(name_and_diff), fmt: fmt }
     }
 }

src/offset/fixed.rs
@@ -1,0 +1,107 @@
+// This is a part of rust-chrono.
+// Copyright (c) 2015, Kang Seonghoon.
+// See README.md and LICENSE.txt for details.
+
+/*!
+ * The time zone which has a fixed offset from UTC.
+ */
+
+use std::fmt;
+
+use div::div_mod_floor;
+use duration::Duration;
+use naive::date::NaiveDate;
+use naive::time::NaiveTime;
+use naive::datetime::NaiveDateTime;
+use super::{Offset, OffsetState, LocalResult};
+
+/// The fixed offset (and also state), from UTC-23:59:59 to UTC+23:59:59.
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub struct FixedOffset {
+    local_minus_utc: i32,
+}
+
+impl FixedOffset {
+    /// Makes a new `FixedOffset` for the Eastern Hemisphere with given timezone difference.
+    /// The negative `secs` means the Western Hemisphere.
+    ///
+    /// Fails on the out-of-bound `secs`.
+    pub fn east(secs: i32) -> FixedOffset {
+        FixedOffset::east_opt(secs).expect("FixedOffset::east out of bounds")
+    }
+
+    /// Makes a new `FixedOffset` for the Eastern Hemisphere with given timezone difference.
+    /// The negative `secs` means the Western Hemisphere.
+    ///
+    /// Returns `None` on the out-of-bound `secs`.
+    pub fn east_opt(secs: i32) -> Option<FixedOffset> {
+        if -86400 < secs && secs < 86400 {
+            Some(FixedOffset { local_minus_utc: secs })
+        } else {
+            None
+        }
+    }
+
+    /// Makes a new `FixedOffset` for the Western Hemisphere with given timezone difference.
+    /// The negative `secs` means the Eastern Hemisphere.
+    ///
+    /// Fails on the out-of-bound `secs`.
+    pub fn west(secs: i32) -> FixedOffset {
+        FixedOffset::west_opt(secs).expect("FixedOffset::west out of bounds")
+    }
+
+    /// Makes a new `FixedOffset` for the Western Hemisphere with given timezone difference.
+    /// The negative `secs` means the Eastern Hemisphere.
+    ///
+    /// Returns `None` on the out-of-bound `secs`.
+    pub fn west_opt(secs: i32) -> Option<FixedOffset> {
+        if -86400 < secs && secs < 86400 {
+            Some(FixedOffset { local_minus_utc: -secs })
+        } else {
+            None
+        }
+    }
+}
+
+impl Offset for FixedOffset {
+    type State = FixedOffset;
+
+    fn from_state(state: &FixedOffset) -> FixedOffset { state.clone() }
+
+    fn state_from_local_date(&self, _local: &NaiveDate) -> LocalResult<FixedOffset> {
+        LocalResult::Single(self.clone())
+    }
+    fn state_from_local_time(&self, _local: &NaiveTime) -> LocalResult<FixedOffset> {
+        LocalResult::Single(self.clone())
+    }
+    fn state_from_local_datetime(&self, _local: &NaiveDateTime) -> LocalResult<FixedOffset> {
+        LocalResult::Single(self.clone())
+    }
+
+    fn state_from_utc_date(&self, _utc: &NaiveDate) -> FixedOffset { self.clone() }
+    fn state_from_utc_time(&self, _utc: &NaiveTime) -> FixedOffset { self.clone() }
+    fn state_from_utc_datetime(&self, _utc: &NaiveDateTime) -> FixedOffset { self.clone() }
+}
+
+impl OffsetState for FixedOffset {
+    fn local_minus_utc(&self) -> Duration { Duration::seconds(self.local_minus_utc as i64) }
+}
+
+impl fmt::Show for FixedOffset {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let offset = self.local_minus_utc;
+        let (sign, offset) = if offset < 0 {('-', -offset)} else {('+', offset)};
+        let (mins, sec) = div_mod_floor(offset, 60);
+        let (hour, min) = div_mod_floor(mins, 60);
+        if sec == 0 {
+            write!(f, "{}{:02}:{:02}", sign, hour, min)
+        } else {
+            write!(f, "{}{:02}:{:02}:{:02}", sign, hour, min, sec)
+        }
+    }
+}
+
+impl fmt::String for FixedOffset {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { fmt::Show::fmt(self, f) }
+}
+

src/offset/utc.rs
@@ -1,0 +1,68 @@
+// This is a part of rust-chrono.
+// Copyright (c) 2015, Kang Seonghoon.
+// See README.md and LICENSE.txt for details.
+
+/*!
+ * The UTC (Coordinated Universal Time) time zone.
+ */
+
+use std::fmt;
+use stdtime;
+
+use duration::Duration;
+use naive::date::NaiveDate;
+use naive::time::NaiveTime;
+use naive::datetime::NaiveDateTime;
+use date::Date;
+use datetime::DateTime;
+use super::{Offset, OffsetState, LocalResult};
+
+/// The UTC offset. This is the most efficient offset when you don't need the local time.
+/// It is also used as an offset state (which is also a dummy type).
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub struct UTC;
+
+impl UTC {
+    /// Returns a `Date` which corresponds to the current date.
+    pub fn today() -> Date<UTC> { UTC::now().date() }
+
+    /// Returns a `DateTime` which corresponds to the current date.
+    pub fn now() -> DateTime<UTC> {
+        let spec = stdtime::get_time();
+        let naive = NaiveDateTime::from_num_seconds_from_unix_epoch(spec.sec, spec.nsec as u32);
+        DateTime::from_utc(naive, UTC)
+    }
+}
+
+impl Offset for UTC {
+    type State = UTC;
+
+    fn from_state(_state: &UTC) -> UTC { UTC }
+
+    fn state_from_local_date(&self, _local: &NaiveDate) -> LocalResult<UTC> {
+        LocalResult::Single(UTC)
+    }
+    fn state_from_local_time(&self, _local: &NaiveTime) -> LocalResult<UTC> {
+        LocalResult::Single(UTC)
+    }
+    fn state_from_local_datetime(&self, _local: &NaiveDateTime) -> LocalResult<UTC> {
+        LocalResult::Single(UTC)
+    }
+
+    fn state_from_utc_date(&self, _utc: &NaiveDate) -> UTC { UTC }
+    fn state_from_utc_time(&self, _utc: &NaiveTime) -> UTC { UTC }
+    fn state_from_utc_datetime(&self, _utc: &NaiveDateTime) -> UTC { UTC}
+}
+
+impl OffsetState for UTC {
+    fn local_minus_utc(&self) -> Duration { Duration::zero() }
+}
+
+impl fmt::Show for UTC {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "Z") }
+}
+
+impl fmt::String for UTC {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "UTC") }
+}
+

src/time.rs
@@ -11,102 +11,119 @@ use std::cmp::Ordering;
 use std::ops::{Add, Sub};
 
 use Timelike;
-use offset::Offset;
+use offset::{Offset, OffsetState};
 use duration::Duration;
 use naive::time::NaiveTime;
 use format::DelayedFormat;
 
 /// ISO 8601 time with timezone.
 #[derive(Clone)]
-pub struct Time<Off> {
+pub struct Time<Off: Offset> {
     time: NaiveTime,
-    offset: Off,
+    offset: Off::State,
 }
 
-impl<Off:Offset> Time<Off> {
+impl<Off: Offset> Time<Off> {
     /// Makes a new `Time` with given *UTC* time and offset.
     /// The local time should be constructed via the `Offset` trait.
+    //
+    // note: this constructor is purposedly not named to `new` to discourage the direct usage.
     #[inline]
-    pub fn from_utc(time: NaiveTime, offset: Off) -> Time<Off> {
+    pub fn from_utc(time: NaiveTime, offset: Off::State) -> Time<Off> {
         Time { time: time, offset: offset }
     }
 
     /// Retrieves an associated offset.
     #[inline]
-    pub fn offset<'a>(&'a self) -> &'a Off {
+    pub fn offset<'a>(&'a self) -> &'a Off::State {
         &self.offset
+    }
+
+    /// Retrieves an associated offset.
+    #[inline]
+    pub fn timezone(&self) -> Off {
+        Offset::from_state(&self.offset)
     }
 
     /// Changes the associated offset.
     /// This does not change the actual `Time` (but will change the string representation).
     #[inline]
-    pub fn with_offset<Off2:Offset>(&self, offset: Off2) -> Time<Off2> {
-        Time::from_utc(self.time, offset)
+    pub fn with_timezone<Off2: Offset>(&self, tz: &Off2) -> Time<Off2> {
+        tz.from_utc_time(&self.time)
     }
 
-    /// Returns a view to the local time.
-    fn local(&self) -> NaiveTime {
-        self.offset.to_local_time(&self.time)
+    /// Returns a view to the naive UTC time.
+    #[inline]
+    pub fn naive_utc(&self) -> NaiveTime {
+        self.time
+    }
+
+    /// Returns a view to the naive local time.
+    #[inline]
+    pub fn naive_local(&self) -> NaiveTime {
+        self.time + self.offset.local_minus_utc()
     }
 }
 
-impl<Off: Offset + fmt::String> Time<Off> {
+/// Maps the local time to other time with given conversion function.
+fn map_local<Off: Offset, F>(t: &Time<Off>, mut f: F) -> Option<Time<Off>>
+        where F: FnMut(NaiveTime) -> Option<NaiveTime> {
+    f(t.naive_local()).and_then(|time| t.timezone().from_local_time(&time).single())
+}
+
+impl<Off: Offset> Time<Off> where Off::State: fmt::String {
     /// Formats the time in the specified format string.
     /// See the `format` module on the supported escape sequences.
     #[inline]
     pub fn format<'a>(&'a self, fmt: &'a str) -> DelayedFormat<'a> {
-        DelayedFormat::new_with_offset(None, Some(self.local()), &self.offset, fmt)
+        DelayedFormat::new_with_offset(None, Some(self.naive_local()), &self.offset, fmt)
     }
 }
 
-impl<Off:Offset> Timelike for Time<Off> {
-    #[inline] fn hour(&self) -> u32 { self.local().hour() }
-    #[inline] fn minute(&self) -> u32 { self.local().minute() }
-    #[inline] fn second(&self) -> u32 { self.local().second() }
-    #[inline] fn nanosecond(&self) -> u32 { self.local().nanosecond() }
+impl<Off: Offset> Timelike for Time<Off> {
+    #[inline] fn hour(&self) -> u32 { self.naive_local().hour() }
+    #[inline] fn minute(&self) -> u32 { self.naive_local().minute() }
+    #[inline] fn second(&self) -> u32 { self.naive_local().second() }
+    #[inline] fn nanosecond(&self) -> u32 { self.naive_local().nanosecond() }
 
     #[inline]
     fn with_hour(&self, hour: u32) -> Option<Time<Off>> {
-        self.local().with_hour(hour)
-            .and_then(|time| self.offset.from_local_time(&time).single())
+        map_local(self, |time| time.with_hour(hour))
     }
 
     #[inline]
     fn with_minute(&self, min: u32) -> Option<Time<Off>> {
-        self.local().with_minute(min)
-            .and_then(|time| self.offset.from_local_time(&time).single())
+        map_local(self, |time| time.with_minute(min))
     }
 
     #[inline]
     fn with_second(&self, sec: u32) -> Option<Time<Off>> {
-        self.local().with_second(sec)
-            .and_then(|time| self.offset.from_local_time(&time).single())
+        map_local(self, |time| time.with_second(sec))
     }
 
     #[inline]
     fn with_nanosecond(&self, nano: u32) -> Option<Time<Off>> {
-        self.local().with_nanosecond(nano)
-            .and_then(|time| self.offset.from_local_time(&time).single())
+        map_local(self, |time| time.with_nanosecond(nano))
     }
 
     #[inline]
-    fn num_seconds_from_midnight(&self) -> u32 { self.local().num_seconds_from_midnight() }
+    fn num_seconds_from_midnight(&self) -> u32 { self.naive_local().num_seconds_from_midnight() }
 }
 
-impl<Off:Offset, Off2:Offset> PartialEq<Time<Off2>> for Time<Off> {
+impl<Off: Offset, Off2: Offset> PartialEq<Time<Off2>> for Time<Off> {
     fn eq(&self, other: &Time<Off2>) -> bool { self.time == other.time }
 }
 
-impl<Off:Offset> Eq for Time<Off> {
+impl<Off: Offset> Eq for Time<Off> {
 }
 
-impl<Off:Offset> PartialOrd for Time<Off> {
+impl<Off: Offset> PartialOrd for Time<Off> {
     fn partial_cmp(&self, other: &Time<Off>) -> Option<Ordering> {
         self.time.partial_cmp(&other.time)
     }
 }
 
-impl<Off:Offset> Ord for Time<Off> {
+impl<Off: Offset> Ord for Time<Off> {
     fn cmp(&self, other: &Time<Off>) -> Ordering { self.time.cmp(&other.time) }
 }
 
@@ -114,7 +131,7 @@ impl<Off: Offset, H: hash::Hasher + hash::Writer> hash::Hash<H> for Time<Off> {
     fn hash(&self, state: &mut H) { self.time.hash(state) }
 }
 
-impl<Off:Offset> Add<Duration> for Time<Off> {
+impl<Off: Offset> Add<Duration> for Time<Off> {
     type Output = Time<Off>;
 
     fn add(self, rhs: Duration) -> Time<Off> {
@@ -122,13 +139,13 @@ impl<Off:Offset> Add<Duration> for Time<Off> {
     }
 }
 
-impl<Off:Offset, Off2:Offset> Sub<Time<Off2>> for Time<Off> {
+impl<Off: Offset, Off2: Offset> Sub<Time<Off2>> for Time<Off> {
     type Output = Duration;
 
     fn sub(self, rhs: Time<Off2>) -> Duration { self.time - rhs.time }
 }
 
-impl<Off:Offset> Sub<Duration> for Time<Off> {
+impl<Off: Offset> Sub<Duration> for Time<Off> {
     type Output = Time<Off>;
 
     #[inline]
@@ -137,13 +154,13 @@ impl<Off:Offset> Sub<Duration> for Time<Off> {
 
 impl<Off: Offset> fmt::Show for Time<Off> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}{:?}", self.local(), self.offset)
+        write!(f, "{:?}{:?}", self.naive_local(), self.offset)
     }
 }
 
-impl<Off: Offset + fmt::String> fmt::String for Time<Off> {
+impl<Off: Offset> fmt::String for Time<Off> where Off::State: fmt::String {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}{}", self.local(), self.offset)
+        write!(f, "{}{}", self.naive_local(), self.offset)
     }
 }
 

chronotope/chrono

src/format.rs
@@ -10,7 +10,7 @@ use std::fmt;
 
 use {Datelike, Timelike};
 use duration::Duration;
-use offset::Offset;
+use offset::OffsetState;
 use naive::date::NaiveDate;
 use naive::time::NaiveTime;
 
@@ -198,11 +198,11 @@ impl<'a> DelayedFormat<'a> {
         DelayedFormat { date: date, time: time, off: None, fmt: fmt }
     }
 
-    /// Makes a new `DelayedFormat` value out of local date and time and UTC offset.
+    /// Makes a new `DelayedFormat` value out of local date and time with offset state.
     pub fn new_with_offset<Off>(date: Option<NaiveDate>, time: Option<NaiveTime>,
-                                offset: &Off, fmt: &'a str) -> DelayedFormat<'a>
-            where Off: Offset + fmt::Display {
-        let name_and_diff = (offset.to_string(), offset.local_minus_utc());
+                                state: &Off, fmt: &'a str) -> DelayedFormat<'a>
+            where Off: OffsetState + fmt::Display {
+        let name_and_diff = (state.to_string(), state.local_minus_utc());
         DelayedFormat { date: date, time: time, off: Some(name_and_diff), fmt: fmt }
     }
 }

src/offset/fixed.rs
@@ -1,0 +1,107 @@
+// This is a part of rust-chrono.
+// Copyright (c) 2015, Kang Seonghoon.
+// See README.md and LICENSE.txt for details.
+
+/*!
+ * The time zone which has a fixed offset from UTC.
+ */
+
+use std::fmt;
+
+use div::div_mod_floor;
+use duration::Duration;
+use naive::date::NaiveDate;
+use naive::time::NaiveTime;
+use naive::datetime::NaiveDateTime;
+use super::{Offset, OffsetState, LocalResult};
+
+/// The fixed offset (and also state), from UTC-23:59:59 to UTC+23:59:59.
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub struct FixedOffset {
+    local_minus_utc: i32,
+}
+
+impl FixedOffset {
+    /// Makes a new `FixedOffset` for the Eastern Hemisphere with given timezone difference.
+    /// The negative `secs` means the Western Hemisphere.
+    ///
+    /// Fails on the out-of-bound `secs`.
+    pub fn east(secs: i32) -> FixedOffset {
+        FixedOffset::east_opt(secs).expect("FixedOffset::east out of bounds")
+    }
+
+    /// Makes a new `FixedOffset` for the Eastern Hemisphere with given timezone difference.
+    /// The negative `secs` means the Western Hemisphere.
+    ///
+    /// Returns `None` on the out-of-bound `secs`.
+    pub fn east_opt(secs: i32) -> Option<FixedOffset> {
+        if -86400 < secs && secs < 86400 {
+            Some(FixedOffset { local_minus_utc: secs })
+        } else {
+            None
+        }
+    }
+
+    /// Makes a new `FixedOffset` for the Western Hemisphere with given timezone difference.
+    /// The negative `secs` means the Eastern Hemisphere.
+    ///
+    /// Fails on the out-of-bound `secs`.
+    pub fn west(secs: i32) -> FixedOffset {
+        FixedOffset::west_opt(secs).expect("FixedOffset::west out of bounds")
+    }
+
+    /// Makes a new `FixedOffset` for the Western Hemisphere with given timezone difference.
+    /// The negative `secs` means the Eastern Hemisphere.
+    ///
+    /// Returns `None` on the out-of-bound `secs`.
+    pub fn west_opt(secs: i32) -> Option<FixedOffset> {
+        if -86400 < secs && secs < 86400 {
+            Some(FixedOffset { local_minus_utc: -secs })
+        } else {
+            None
+        }
+    }
+}
+
+impl Offset for FixedOffset {
+    type State = FixedOffset;
+
+    fn from_state(state: &FixedOffset) -> FixedOffset { state.clone() }
+
+    fn state_from_local_date(&self, _local: &NaiveDate) -> LocalResult<FixedOffset> {
+        LocalResult::Single(self.clone())
+    }
+    fn state_from_local_time(&self, _local: &NaiveTime) -> LocalResult<FixedOffset> {
+        LocalResult::Single(self.clone())
+    }
+    fn state_from_local_datetime(&self, _local: &NaiveDateTime) -> LocalResult<FixedOffset> {
+        LocalResult::Single(self.clone())
+    }
+
+    fn state_from_utc_date(&self, _utc: &NaiveDate) -> FixedOffset { self.clone() }
+    fn state_from_utc_time(&self, _utc: &NaiveTime) -> FixedOffset { self.clone() }
+    fn state_from_utc_datetime(&self, _utc: &NaiveDateTime) -> FixedOffset { self.clone() }
+}
+
+impl OffsetState for FixedOffset {
+    fn local_minus_utc(&self) -> Duration { Duration::seconds(self.local_minus_utc as i64) }
+}
+
+impl fmt::Debug for FixedOffset {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let offset = self.local_minus_utc;
+        let (sign, offset) = if offset < 0 {('-', -offset)} else {('+', offset)};
+        let (mins, sec) = div_mod_floor(offset, 60);
+        let (hour, min) = div_mod_floor(mins, 60);
+        if sec == 0 {
+            write!(f, "{}{:02}:{:02}", sign, hour, min)
+        } else {
+            write!(f, "{}{:02}:{:02}:{:02}", sign, hour, min, sec)
+        }
+    }
+}
+
+impl fmt::Display for FixedOffset {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { fmt::Debug::fmt(self, f) }
+}
+

src/offset/utc.rs
@@ -1,0 +1,68 @@
+// This is a part of rust-chrono.
+// Copyright (c) 2015, Kang Seonghoon.
+// See README.md and LICENSE.txt for details.
+
+/*!
+ * The UTC (Coordinated Universal Time) time zone.
+ */
+
+use std::fmt;
+use stdtime;
+
+use duration::Duration;
+use naive::date::NaiveDate;
+use naive::time::NaiveTime;
+use naive::datetime::NaiveDateTime;
+use date::Date;
+use datetime::DateTime;
+use super::{Offset, OffsetState, LocalResult};
+
+/// The UTC offset. This is the most efficient offset when you don't need the local time.
+/// It is also used as an offset state (which is also a dummy type).
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub struct UTC;
+
+impl UTC {
+    /// Returns a `Date` which corresponds to the current date.
+    pub fn today() -> Date<UTC> { UTC::now().date() }
+
+    /// Returns a `DateTime` which corresponds to the current date.
+    pub fn now() -> DateTime<UTC> {
+        let spec = stdtime::get_time();
+        let naive = NaiveDateTime::from_num_seconds_from_unix_epoch(spec.sec, spec.nsec as u32);
+        DateTime::from_utc(naive, UTC)
+    }
+}
+
+impl Offset for UTC {
+    type State = UTC;
+
+    fn from_state(_state: &UTC) -> UTC { UTC }
+
+    fn state_from_local_date(&self, _local: &NaiveDate) -> LocalResult<UTC> {
+        LocalResult::Single(UTC)
+    }
+    fn state_from_local_time(&self, _local: &NaiveTime) -> LocalResult<UTC> {
+        LocalResult::Single(UTC)
+    }
+    fn state_from_local_datetime(&self, _local: &NaiveDateTime) -> LocalResult<UTC> {
+        LocalResult::Single(UTC)
+    }
+
+    fn state_from_utc_date(&self, _utc: &NaiveDate) -> UTC { UTC }
+    fn state_from_utc_time(&self, _utc: &NaiveTime) -> UTC { UTC }
+    fn state_from_utc_datetime(&self, _utc: &NaiveDateTime) -> UTC { UTC}
+}
+
+impl OffsetState for UTC {
+    fn local_minus_utc(&self) -> Duration { Duration::zero() }
+}
+
+impl fmt::Debug for UTC {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "Z") }
+}
+
+impl fmt::Display for UTC {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "UTC") }
+}
+

src/time.rs
@@ -11,102 +11,119 @@ use std::cmp::Ordering;
 use std::ops::{Add, Sub};
 
 use Timelike;
-use offset::Offset;
+use offset::{Offset, OffsetState};
 use duration::Duration;
 use naive::time::NaiveTime;
 use format::DelayedFormat;
 
 /// ISO 8601 time with timezone.
 #[derive(Clone)]
-pub struct Time<Off> {
+pub struct Time<Off: Offset> {
     time: NaiveTime,
-    offset: Off,
+    offset: Off::State,
 }
 
-impl<Off:Offset> Time<Off> {
+impl<Off: Offset> Time<Off> {
     /// Makes a new `Time` with given *UTC* time and offset.
     /// The local time should be constructed via the `Offset` trait.
+    //
+    // note: this constructor is purposedly not named to `new` to discourage the direct usage.
     #[inline]
-    pub fn from_utc(time: NaiveTime, offset: Off) -> Time<Off> {
+    pub fn from_utc(time: NaiveTime, offset: Off::State) -> Time<Off> {
         Time { time: time, offset: offset }
     }
 
     /// Retrieves an associated offset.
     #[inline]
-    pub fn offset<'a>(&'a self) -> &'a Off {
+    pub fn offset<'a>(&'a self) -> &'a Off::State {
         &self.offset
+    }
+
+    /// Retrieves an associated offset.
+    #[inline]
+    pub fn timezone(&self) -> Off {
+        Offset::from_state(&self.offset)
     }
 
     /// Changes the associated offset.
     /// This does not change the actual `Time` (but will change the string representation).
     #[inline]
-    pub fn with_offset<Off2:Offset>(&self, offset: Off2) -> Time<Off2> {
-        Time::from_utc(self.time, offset)
+    pub fn with_timezone<Off2: Offset>(&self, tz: &Off2) -> Time<Off2> {
+        tz.from_utc_time(&self.time)
     }
 
-    /// Returns a view to the local time.
-    fn local(&self) -> NaiveTime {
-        self.offset.to_local_time(&self.time)
+    /// Returns a view to the naive UTC time.
+    #[inline]
+    pub fn naive_utc(&self) -> NaiveTime {
+        self.time
+    }
+
+    /// Returns a view to the naive local time.
+    #[inline]
+    pub fn naive_local(&self) -> NaiveTime {
+        self.time + self.offset.local_minus_utc()
     }
 }
 
-impl<Off: Offset + fmt::Display> Time<Off> {
+/// Maps the local time to other time with given conversion function.
+fn map_local<Off: Offset, F>(t: &Time<Off>, mut f: F) -> Option<Time<Off>>
+        where F: FnMut(NaiveTime) -> Option<NaiveTime> {
+    f(t.naive_local()).and_then(|time| t.timezone().from_local_time(&time).single())
+}
+
+impl<Off: Offset> Time<Off> where Off::State: fmt::Display {
     /// Formats the time in the specified format string.
     /// See the `format` module on the supported escape sequences.
     #[inline]
     pub fn format<'a>(&'a self, fmt: &'a str) -> DelayedFormat<'a> {
-        DelayedFormat::new_with_offset(None, Some(self.local()), &self.offset, fmt)
+        DelayedFormat::new_with_offset(None, Some(self.naive_local()), &self.offset, fmt)
     }
 }
 
-impl<Off:Offset> Timelike for Time<Off> {
-    #[inline] fn hour(&self) -> u32 { self.local().hour() }
-    #[inline] fn minute(&self) -> u32 { self.local().minute() }
-    #[inline] fn second(&self) -> u32 { self.local().second() }
-    #[inline] fn nanosecond(&self) -> u32 { self.local().nanosecond() }
+impl<Off: Offset> Timelike for Time<Off> {
+    #[inline] fn hour(&self) -> u32 { self.naive_local().hour() }
+    #[inline] fn minute(&self) -> u32 { self.naive_local().minute() }
+    #[inline] fn second(&self) -> u32 { self.naive_local().second() }
+    #[inline] fn nanosecond(&self) -> u32 { self.naive_local().nanosecond() }
 
     #[inline]
     fn with_hour(&self, hour: u32) -> Option<Time<Off>> {
-        self.local().with_hour(hour)
-            .and_then(|time| self.offset.from_local_time(&time).single())
+        map_local(self, |time| time.with_hour(hour))
     }
 
     #[inline]
     fn with_minute(&self, min: u32) -> Option<Time<Off>> {
-        self.local().with_minute(min)
-            .and_then(|time| self.offset.from_local_time(&time).single())
+        map_local(self, |time| time.with_minute(min))
     }
 
     #[inline]
     fn with_second(&self, sec: u32) -> Option<Time<Off>> {
-        self.local().with_second(sec)
-            .and_then(|time| self.offset.from_local_time(&time).single())
+        map_local(self, |time| time.with_second(sec))
     }
 
     #[inline]
     fn with_nanosecond(&self, nano: u32) -> Option<Time<Off>> {
-        self.local().with_nanosecond(nano)
-            .and_then(|time| self.offset.from_local_time(&time).single())
+        map_local(self, |time| time.with_nanosecond(nano))
     }
 
     #[inline]
-    fn num_seconds_from_midnight(&self) -> u32 { self.local().num_seconds_from_midnight() }
+    fn num_seconds_from_midnight(&self) -> u32 { self.naive_local().num_seconds_from_midnight() }
 }
 
-impl<Off:Offset, Off2:Offset> PartialEq<Time<Off2>> for Time<Off> {
+impl<Off: Offset, Off2: Offset> PartialEq<Time<Off2>> for Time<Off> {
     fn eq(&self, other: &Time<Off2>) -> bool { self.time == other.time }
 }
 
-impl<Off:Offset> Eq for Time<Off> {
+impl<Off: Offset> Eq for Time<Off> {
 }
 
-impl<Off:Offset> PartialOrd for Time<Off> {
+impl<Off: Offset> PartialOrd for Time<Off> {
     fn partial_cmp(&self, other: &Time<Off>) -> Option<Ordering> {
         self.time.partial_cmp(&other.time)
     }
 }
 
-impl<Off:Offset> Ord for Time<Off> {
+impl<Off: Offset> Ord for Time<Off> {
     fn cmp(&self, other: &Time<Off>) -> Ordering { self.time.cmp(&other.time) }
 }
 
@@ -114,7 +131,7 @@ impl<Off: Offset, H: hash::Hasher + hash::Writer> hash::Hash<H> for Time<Off> {
     fn hash(&self, state: &mut H) { self.time.hash(state) }
 }
 
-impl<Off:Offset> Add<Duration> for Time<Off> {
+impl<Off: Offset> Add<Duration> for Time<Off> {
     type Output = Time<Off>;
 
     fn add(self, rhs: Duration) -> Time<Off> {
@@ -122,13 +139,13 @@ impl<Off:Offset> Add<Duration> for Time<Off> {
     }
 }
 
-impl<Off:Offset, Off2:Offset> Sub<Time<Off2>> for Time<Off> {
+impl<Off: Offset, Off2: Offset> Sub<Time<Off2>> for Time<Off> {
     type Output = Duration;
 
     fn sub(self, rhs: Time<Off2>) -> Duration { self.time - rhs.time }
 }
 
-impl<Off:Offset> Sub<Duration> for Time<Off> {
+impl<Off: Offset> Sub<Duration> for Time<Off> {
     type Output = Time<Off>;
 
     #[inline]
@@ -137,13 +154,13 @@ impl<Off:Offset> Sub<Duration> for Time<Off> {
 
 impl<Off: Offset> fmt::Debug for Time<Off> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}{:?}", self.local(), self.offset)
+        write!(f, "{:?}{:?}", self.naive_local(), self.offset)
     }
 }
 
-impl<Off: Offset + fmt::Display> fmt::Display for Time<Off> {
+impl<Off: Offset> fmt::Display for Time<Off> where Off::State: fmt::Display {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}{}", self.local(), self.offset)
+        write!(f, "{}{}", self.naive_local(), self.offset)
     }
 }
 