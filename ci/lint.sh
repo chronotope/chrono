c788a4495b5bde050d1e4a52e5b4a53d56c51058
Brandon W Maister <quodlibetor@gmail.com>
2020-06-27 17:10:38 -0400

    Add a lint for cargo fmt
    This also adds the cargo fmt commit to the (new) .git-ignore-revs file which
    can cause it to not show up in `git blame` either with `--ignore-revs-file` or
    `git config blame.ignoreRevsFile .git-ignore-revs`

--- ci/lint.sh
+++ ci/lint.sh
@@ -8,6 +8,7 @@ source "${BASH_SOURCE[0]%/*}/_shlib.sh"
 main() {
     # rustflags are present because of: https://github.com/rust-lang/rust-clippy/issues/5749
     runt env RUSTFLAGS="-Dwarnings" cargo clippy --color=always -- -D warnings
+    runt cargo fmt -- --check --color=always
     runt make readme
     runv git diff --exit-code -- README.md
 }
