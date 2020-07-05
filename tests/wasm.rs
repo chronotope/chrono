b6d7bac99ac153efe95dd3871da73721de966e22
Brandon W Maister <quodlibetor@gmail.com>
2020-07-05 15:55:13 -0400

    Fix wasm testing
    wasm-bindgen-test and wasm-bindgen need to be different versions (0.3 and 0.2,
    respectively), otherwise no tests are found([1], [2]);
    
    This also fixes the test to and ci runner to just test in the current env/tz,
    thereby requiring that js_sys returns the same timezone as the host system
    thinks it is, but not otherwise trying to set the TZ.
    
    [1]: https://users.rust-lang.org/t/wasm-bindgen-test-and-no-tests-to-run/43444
    [2]: https://github.com/rustwasm/wasm-bindgen/pull/2123

--- tests/wasm.rs
+++ tests/wasm.rs
@@ -12,17 +12,27 @@ mod test {
         let local: DateTime<Local> = Local::now();
 
         // Ensure time fetched is correct
-        let actual = Utc.datetime_from_str(env!("NOW"), "%s").unwrap();
+        let actual = Utc.datetime_from_str(&env!("NOW"), "%s").unwrap();
         assert!(utc - actual < chrono::Duration::minutes(5));
 
+        let tz = env!("TZ");
+        eprintln!("testing with tz={}", tz);
+
         // Ensure offset retrieved when getting local time is correct
-        let expected_offset = match env!("TZ") {
+        let expected_offset = match tz {
             "ACST-9:30" => FixedOffset::east(19 * 30 * 60),
             "Asia/Katmandu" => FixedOffset::east(23 * 15 * 60), // No DST thankfully
-            "EST4" => FixedOffset::east(-4 * 60 * 60),
-            "UTC0" => FixedOffset::east(0),
-            _ => panic!("unexpected TZ"),
+            "EDT" | "EST4" | "-0400" => FixedOffset::east(-4 * 60 * 60),
+            "EST" | "-0500" => FixedOffset::east(-5 * 60 * 60),
+            "UTC0" | "+0000" => FixedOffset::east(0),
+            tz => panic!("unexpected TZ {}", tz),
         };
-        assert_eq!(&expected_offset, local.offset());
+        assert_eq!(
+            &expected_offset,
+            local.offset(),
+            "expected: {:?} local: {:?}",
+            expected_offset,
+            local.offset(),
+        );
     }
 }
