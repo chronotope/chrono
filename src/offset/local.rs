31e3e1602e92f78308fffbdcc6927fa3ddd7eec0
ShadoySV <shadoysv@yandex.ru>
2020-08-05 13:31:25 +0300

    Fix for test

--- src/offset/local.rs
+++ src/offset/local.rs
@@ -3,7 +3,6 @@
 
 //! The local (system) time zone.
 
-#[cfg(not(all(target_arch = "wasm32", not(target_os = "wasi"), feature = "wasmbind")))]
 use oldtime;
 
 use super::fixed::FixedOffset;
@@ -152,7 +151,7 @@ impl TimeZone for Local {
         let mut local = local.clone();
         // Get the offset from the js runtime
         let offset = FixedOffset::west((js_sys::Date::new_0().get_timezone_offset() as i32) * 60);
-        local -= crate::Duration::seconds(offset.local_minus_utc() as i64);
+        local -= oldtime::Duration::seconds(offset.local_minus_utc() as i64);
         LocalResult::Single(DateTime::from_utc(local, offset))
     }
 
