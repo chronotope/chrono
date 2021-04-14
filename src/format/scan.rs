827a0999f6fe514cf1166bd6e74282268c0d6386
Daniel Lim <orcas@octalorca.me>
2021-04-14 22:15:04 +0800

    forgot to normalise case in checks

--- src/format/scan.rs
+++ src/format/scan.rs
@@ -152,7 +152,7 @@ pub fn short_or_long_month0(s: &str) -> ParseResult<(&str, u8)> {
 
     // tries to consume the suffix if possible
     let suffix = LONG_MONTH_SUFFIXES[month0 as usize];
-    if s.chars().count() >= suffix.chars().count() && s.chars().zip(suffix.chars()).all(|(a, b)| a == b) {
+    if s.chars().count() >= suffix.chars().count() && s.chars().zip(suffix.chars()).all(|(a, b)| a.to_ascii_lowercase() == b.to_ascii_lowercase()) {
         // At this point, the parts that matter are guaranteed to be ASCII
         s = &s[suffix.len()..];
     }
@@ -171,7 +171,7 @@ pub fn short_or_long_weekday(s: &str) -> ParseResult<(&str, Weekday)> {
 
     // tries to consume the suffix if possible
     let suffix = LONG_WEEKDAY_SUFFIXES[weekday.num_days_from_monday() as usize];
-    if s.chars().count() >= suffix.chars().count() && s.chars().zip(suffix.chars()).all(|(a, b)| a == b) {
+    if s.chars().count() >= suffix.chars().count() && s.chars().zip(suffix.chars()).all(|(a, b)| a.to_ascii_lowercase() == b.to_ascii_lowercase()) {
         // At this point, the parts that matter are guaranteed to be ASCII
         s = &s[suffix.len()..];
     }
