0745dde03821704f6d659912f022ff45db6f9117
Milo <50248166+Milo123459@users.noreply.github.com>
2021-12-29 22:25:02 +0000

    make something pass

--- src/round.rs
+++ src/round.rs
@@ -5,7 +5,6 @@ use core::cmp::Ordering;
 use core::fmt;
 use core::marker::Sized;
 use core::ops::{Add, Sub};
-use std::convert::TryInto;
 use datetime::DateTime;
 use naive::NaiveDateTime;
 use oldtime::Duration;
@@ -213,19 +212,19 @@ fn duration_trunc<T>(
 where
     T: Timelike + Add<Duration, Output = T> + Sub<Duration, Output = T>,
 {
-    let span = duration.whole_nanoseconds();
+    let span = duration.whole_nanoseconds() as i64;
     if naive.timestamp().abs() > MAX_SECONDS_TIMESTAMP_FOR_NANOS {
         return Err(RoundingError::TimestampExceedsLimit);
     }
     let stamp = naive.timestamp_nanos();
-    if span > stamp.abs().into() {
+    if span > stamp.abs() {
         return Err(RoundingError::DurationExceedsTimestamp);
     }
-    let delta_down = stamp % span as i64;
+    let delta_down = stamp % span;
     match delta_down.cmp(&0) {
         Ordering::Equal => Ok(original),
         Ordering::Greater => Ok(original - Duration::nanoseconds(delta_down)),
-        Ordering::Less => Ok(original - Duration::nanoseconds((span - (delta_down.abs() as i128)).try_into().unwrap())),
+        Ordering::Less => Ok(original - Duration::nanoseconds(span - delta_down.abs())),
     }
 }
 
