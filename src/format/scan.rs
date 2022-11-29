2e505f2e7d91b2821668f7ee58aebe4549e77e67
LingMan <LingMan@users.noreply.github.com>
2022-11-29 04:18:06 +0100

    Use `is_ascii_alphabetic` in `timezone_offset_2822`
    Fixes clippy warning:
    ```
    warning: match expression looks like `matches!` macro
       --> src/format/scan.rs:316:24
        |
    316 |           .position(|&c| match c {
        |  ________________________^
    317 | |             b'a'..=b'z' | b'A'..=b'Z' => false,
    318 | |             _ => true,
    319 | |         })
        | |_________^ help: try this: `!matches!(c, b'a'..=b'z' | b'A'..=b'Z')`
        |
        = note: `#[warn(clippy::match_like_matches_macro)]` on by default
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#match_like_matches_macro
    ```

--- src/format/scan.rs
+++ src/format/scan.rs
@@ -310,14 +310,7 @@ where
 /// May return `None` which indicates an insufficient offset data (i.e. `-0000`).
 pub(super) fn timezone_offset_2822(s: &str) -> ParseResult<(&str, Option<i32>)> {
     // tries to parse legacy time zone names
-    let upto = s
-        .as_bytes()
-        .iter()
-        .position(|&c| match c {
-            b'a'..=b'z' | b'A'..=b'Z' => false,
-            _ => true,
-        })
-        .unwrap_or(s.len());
+    let upto = s.as_bytes().iter().position(|c| !c.is_ascii_alphabetic()).unwrap_or(s.len());
     if upto > 0 {
         let name = &s[..upto];
         let s = &s[upto..];
