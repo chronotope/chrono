6cb69fbdedb5e47394ddf7aaf1cf911325ac8df3
Paul Dicker <pitdicker@gmail.com>
2023-05-19 14:20:40 +0200

    Use `add_days()` for `checked_sub_signed()`

--- src/naive/date.rs
+++ src/naive/date.rs
@@ -1156,16 +1156,8 @@ impl NaiveDate {
     /// ```
     #[must_use]
     pub fn checked_sub_signed(self, rhs: OldDuration) -> Option<NaiveDate> {
-        let year = self.year();
-        let (mut year_div_400, year_mod_400) = div_mod_floor(year, 400);
-        let cycle = internals::yo_to_cycle(year_mod_400 as u32, self.of().ordinal());
-        let cycle = (cycle as i32).checked_sub(i32::try_from(rhs.num_days()).ok()?)?;
-        let (cycle_div_400y, cycle) = div_mod_floor(cycle, 146_097);
-        year_div_400 += cycle_div_400y;
-
-        let (year_mod_400, ordinal) = internals::cycle_to_yo(cycle as u32);
-        let flags = YearFlags::from_year_mod_400(year_mod_400 as i32);
-        NaiveDate::from_ordinal_and_flags(year_div_400 * 400 + year_mod_400 as i32, ordinal, flags)
+        let days = i32::try_from(-rhs.num_days()).ok()?;
+        self.add_days(days)
     }
 
     /// Subtracts another `NaiveDate` from the current date.
