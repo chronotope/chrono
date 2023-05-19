bcf905761a36168b139d0543a5e6743788b80ca7
Paul Dicker <paul@pitdicker.nl>
2023-05-19 15:29:42 +0200

    Support encoding `-00:00` in `FixedOffset`

--- src/offset/fixed.rs
+++ src/offset/fixed.rs
@@ -4,6 +4,7 @@
 //! The time zone which has a fixed offset from UTC.
 
 use core::fmt;
+use core::num::NonZeroI32;
 use core::str::FromStr;
 
 #[cfg(any(feature = "rkyv", feature = "rkyv-16", feature = "rkyv-32", feature = "rkyv-64"))]
@@ -13,13 +14,16 @@ use super::{LocalResult, Offset, TimeZone};
 use crate::format::{scan, ParseError, OUT_OF_RANGE};
 use crate::naive::{NaiveDate, NaiveDateTime};
 
+const OFFSET_NORMAL: i32 = 1;
+const OFFSET_UNKNOWN: i32 = 2;
+
 /// The time zone with fixed offset, from UTC-23:59:59 to UTC+23:59:59.
 ///
 /// Using the [`TimeZone`](./trait.TimeZone.html) methods
 /// on a `FixedOffset` struct is the preferred way to construct
 /// `DateTime<FixedOffset>` instances. See the [`east_opt`](#method.east_opt) and
 /// [`west_opt`](#method.west_opt) methods for examples.
-#[derive(PartialEq, Eq, Hash, Copy, Clone)]
+#[derive(Eq, Copy, Clone)]
 #[cfg_attr(
     any(feature = "rkyv", feature = "rkyv-16", feature = "rkyv-32", feature = "rkyv-64"),
     derive(Archive, Deserialize, Serialize),
@@ -28,7 +32,41 @@ use crate::naive::{NaiveDate, NaiveDateTime};
 )]
 #[cfg_attr(feature = "rkyv-validation", archive(check_bytes))]
 pub struct FixedOffset {
-    local_minus_utc: i32,
+    // Encodes an offset in seconds, with the value shifted two bits to the left.
+    // The remaining bits encode flags:
+    // - `OFFSET_NORMAL` sets one flag to a non-zero value, so we can encode it in a `NonZeroI32`
+    //   and get niche optimization.
+    // - `OFFSET_UNKNOWN` to preserve the difference RFC 2822 and RFC 3339 make between an offset
+    //    of +0:00 and -0:00.
+    // Advantage of this encoding is that it only takes a single shift right to get offset in
+    // seconds.
+    //
+    // Use `local_minus_utc()` to get the offset in seconds, and `no_offset_info()` to inspect the
+    // `OFFSET_UNKNOWN` flag.
+    local_minus_utc: NonZeroI32,
+}
+
+// Compare with only the offset. `-00:00` compares equal to `+00:00`
+impl PartialEq for FixedOffset {
+    fn eq(&self, other: &Self) -> bool {
+        self.local_minus_utc() == other.local_minus_utc()
+    }
+}
+
+// Hash only the offset. `-00:00` hashes equal to `+00:00`
+impl core::hash::Hash for FixedOffset {
+    fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
+        self.local_minus_utc().hash(state);
+    }
+}
+
+// Workaround because `unwrap` in `NonZeroI32::new(val).unwrap()` is not const.
+#[allow(unconditional_panic)]
+const fn nonzero_i32_new(val: i32) -> NonZeroI32 {
+    match NonZeroI32::new(val) {
+        Some(v) => v,
+        None => [][0],
+    }
 }
 
 impl FixedOffset {
@@ -60,7 +98,7 @@ impl FixedOffset {
     #[must_use]
     pub const fn east_opt(secs: i32) -> Option<FixedOffset> {
         if -86_400 < secs && secs < 86_400 {
-            Some(FixedOffset { local_minus_utc: secs })
+            Some(FixedOffset { local_minus_utc: nonzero_i32_new(secs << 2 | OFFSET_NORMAL) })
         } else {
             None
         }
@@ -94,7 +132,7 @@ impl FixedOffset {
     #[must_use]
     pub const fn west_opt(secs: i32) -> Option<FixedOffset> {
         if -86_400 < secs && secs < 86_400 {
-            Some(FixedOffset { local_minus_utc: -secs })
+            Some(FixedOffset { local_minus_utc: nonzero_i32_new(-secs << 2 | OFFSET_NORMAL) })
         } else {
             None
         }
@@ -103,7 +141,7 @@ impl FixedOffset {
     /// Returns the number of seconds to add to convert from UTC to the local time.
     #[inline]
     pub const fn local_minus_utc(&self) -> i32 {
-        self.local_minus_utc
+        self.local_minus_utc.get() >> 2
     }
 
     /// Returns the number of seconds to add to convert from the local time to UTC.
@@ -111,6 +149,21 @@ impl FixedOffset {
     pub const fn utc_minus_local(&self) -> i32 {
         -self.local_minus_utc()
     }
+
+    /// Returns true if this `FixedOffset` contains no offset data (in some formats encoded as
+    /// `-00:00`).
+    ///
+    /// In some formats, such as RFC 2822 and RFC 3339, `-00:00` can represent timestamps for which
+    /// only the time in UTC is known, and the relation to local time is not avaliable (anymore).
+    #[inline]
+    pub const fn no_offset_info(&self) -> bool {
+        self.local_minus_utc.get() == OFFSET_UNKNOWN
+    }
+
+    /// A special value to indicate no offset information is available.
+    /// The created offset will have the value `-00:00`.
+    pub const OFFSET_UNKNOWN: FixedOffset =
+        FixedOffset { local_minus_utc: nonzero_i32_new(OFFSET_UNKNOWN) };
 }
 
 /// Parsing a `str` into a `FixedOffset` uses the format [`%z`](crate::format::strftime).
@@ -152,6 +205,11 @@ impl Offset for FixedOffset {
 
 impl fmt::Debug for FixedOffset {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.no_offset_info() {
+            // In RFC 3339 `-00:00` can represent timestamps for which only the time in UTC is
+            // known, and the relation to local time is not avaliable (anymore).
+            return write!(f, "-00:00");
+        }
         let offset = self.local_minus_utc();
         let (sign, offset) = if offset < 0 { ('-', -offset) } else { ('+', offset) };
         let sec = offset.rem_euclid(60);
