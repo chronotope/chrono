aa8a23785e2cc71352ab3e123bf043cb63c55c0d
Dom Dwyer <dom@itsallbroken.com>
2023-06-08 15:27:23 +0200

    fix: no panic when formatting with %#z
    This changes the "don't use me" panic into an error when using the
    parsing-only "%#z" formatter, consistent with other invalid formatters.

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -675,7 +675,7 @@ fn format_inner(
                         off.map(|&(_, off)| write_local_minus_utc(result, off, true, Colons::None))
                     }
                     Internal(InternalFixed { val: InternalInternal::TimezoneOffsetPermissive }) => {
-                        panic!("Do not try to write %#z it is undefined")
+                        return Err(fmt::Error);
                     }
                     RFC2822 =>
                     // same as `%a, %d %b %Y %H:%M:%S %z`
