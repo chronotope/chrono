8aa2db946d2c4161841d783207be3564808a8f4e
Paul Dicker <paul@pitdicker.nl>
2023-06-09 19:50:30 +0200

    Add helper type for parsing fractions

--- src/format/parse_iso8601.rs
+++ src/format/parse_iso8601.rs
@@ -99,6 +99,74 @@ fn parse_iso8601_year(mut s: &str) -> ParseResult<(&str, i64)> {
     }
 }
 
+/// Helper type for parsing fractional numbers.
+///
+/// The fractions is stored as an integer in the range 0..=10^15.
+/// With this limit `10^15 * 3600` fits in an `u64` without overflow.
+///
+// We don't use `f64` to support targets that may not have floating point support.
+struct Fraction(u64);
+
+impl Fraction {
+    /// Supported formats are `,fraction` and `.fraction`.
+    /// `fraction` can have an unlimited length. We only keep the first 15 digits, and look at the
+    /// 16th digit for correct rounding.
+    fn parse(mut s: &str) -> Option<(&str, Self)> {
+        s = match s.as_bytes().first() {
+            Some(&b',' | &b'.') => &s[1..],
+            _ => return None,
+        };
+        let digits_in_fraction = s.as_bytes().iter().take_while(|c| c.is_ascii_digit()).count();
+        let mut fraction = scan::number(s, 1, 15).map(|(_, f)| f).ok()? as u64;
+        if digits_in_fraction <= 15 {
+            fraction *= POW10[15 - digits_in_fraction];
+        } else if s.as_bytes()[15] >= b'5' {
+            fraction += 1;
+        }
+        s = &s[digits_in_fraction..];
+        Some((s, Fraction(fraction)))
+    }
+
+    /// Returns the result of multiplying this `Fraction` with `unit`.
+    ///
+    /// Rounds to the nearest integer.
+    fn mul(&self, unit: u64) -> i64 {
+        assert!(unit <= 3600); // assumption to prevent overflow later.
+        ((self.0 * unit + (POW10[15] / 2 - 1)) / POW10[15]) as i64
+    }
+
+    /// Returns the result of multiplying this `Fraction` with `unit`.
+    ///
+    /// Returns two integers to represent the whole number and the fraction as nanos.
+    fn mul_with_nanos(&self, unit: u64) -> (i64, i64) {
+        assert!(unit <= 3600); // assumption to prevent overflow later.
+        let div = POW10[15 - 9];
+        let huge = self.0 * unit + (div / 2 - 1);
+        let whole = huge / POW10[15];
+        let fraction_as_nanos = (huge % POW10[15]) / div;
+        (whole as i64, fraction_as_nanos as i64)
+    }
+}
+
+const POW10: [u64; 16] = [
+    1, // unused, for easy indexing
+    10,
+    100,
+    1_000,
+    10_000,
+    100_000,
+    1_000_000,
+    10_000_000,
+    100_000_000,
+    1_000_000_000,
+    10_000_000_000,
+    100_000_000_000,
+    1_000_000_000_000,
+    10_000_000_000_000,
+    100_000_000_000_000,
+    1_000_000_000_000_000,
+];
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -181,5 +249,20 @@ mod tests {
         assert_eq!(parse("23-W23-5 "), Err(INVALID));
         // Year is interpreted as `iso_year`
         assert_eq!(parse("2022-W52-7 "), Ok((NaiveDate::from_ymd_opt(2023, 1, 1).unwrap(), " ")));
+    }
+
+    #[test]
+    fn test_parse_fraction() {
+        let (_, fraction) = Fraction::parse(",123").unwrap();
+        assert_eq!(fraction.0, 123_000_000_000_000);
+        let (_, fraction) = Fraction::parse(",123456789012345").unwrap();
+        assert_eq!(fraction.0, 123_456_789_012_345);
+        let (_, fraction) = Fraction::parse(",1234567890123454").unwrap();
+        assert_eq!(fraction.0, 123_456_789_012_345);
+        let (_, fraction) = Fraction::parse(",1234567890123455").unwrap();
+        assert_eq!(fraction.0, 123_456_789_012_346);
+
+        let (_, fraction) = Fraction::parse(",5").unwrap();
+        assert_eq!(fraction.mul_with_nanos(1), (0, 500_000_000));
     }
 }
