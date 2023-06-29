5293ed77467620a11008d0734c1a72939262b103
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-06-29 13:48:58 +0200

    Fold zulu parsing into timezone_offset()

--- src/offset/fixed.rs
+++ src/offset/fixed.rs
@@ -118,7 +118,7 @@ impl FixedOffset {
 impl FromStr for FixedOffset {
     type Err = ParseError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (_, offset) = scan::timezone_offset(s, scan::colon_or_space, false, true)?;
+        let (_, offset) = scan::timezone_offset(s, scan::colon_or_space, false, false, true)?;
         Self::east_opt(offset).ok_or(OUT_OF_RANGE)
     }
 }
