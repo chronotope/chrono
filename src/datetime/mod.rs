e9a0e296d575720b92c8bbe8eeb7fe4d463e9759
Paul Dicker <paul@pitdicker.nl>
2023-09-06 20:41:44 +0200

    Make `From<DateTime<Utc>> for js_sys::Date` more generic

--- src/datetime/mod.rs
+++ src/datetime/mod.rs
@@ -1412,11 +1412,11 @@ impl From<&js_sys::Date> for DateTime<Utc> {
         not(any(target_os = "emscripten", target_os = "wasi"))
     )))
 )]
-impl From<DateTime<Utc>> for js_sys::Date {
-    /// Converts a `DateTime<Utc>` to a JS `Date`. The resulting value may be lossy,
+impl<Tz: TimeZone> From<DateTime<Tz>> for js_sys::Date {
+    /// Converts a `DateTime<Tz>` to a JS `Date`. The resulting value may be lossy,
     /// any values that have a millisecond timestamp value greater/less than ±8,640,000,000,000,000
     /// (April 20, 271821 BCE ~ September 13, 275760 CE) will become invalid dates in JS.
-    fn from(date: DateTime<Utc>) -> js_sys::Date {
+    fn from(date: DateTime<Tz>) -> js_sys::Date {
         let js_millis = wasm_bindgen::JsValue::from_f64(date.timestamp_millis() as f64);
         js_sys::Date::new(&js_millis)
     }
