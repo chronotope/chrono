5db253117b4f6223ef62954141b0a42eabc1d171
Paul Dicker <paul@pitdicker.nl>
2023-09-08 07:25:49 +0200

    Add `date!` macro

--- src/lib.rs
+++ src/lib.rs
@@ -521,6 +521,8 @@ pub use time_delta::TimeDelta;
 /// Alias of [`TimeDelta`].
 pub type Duration = TimeDelta;
 
+mod macros;
+
 use core::fmt;
 
 /// A convenience module appropriate for glob imports (`use chrono::prelude::*;`).
