742bc778f11a6d0811b47352b6b507381522f0a1
Paul Dicker <paul@pitdicker.nl>
2023-09-24 08:39:46 +0200

    Add `datetime!` macro

--- src/macros.rs
+++ src/macros.rs
@@ -83,10 +83,58 @@ macro_rules! time {
     }};
 }
 
+/// Create a [`NaiveDateTime`](crate::naive::NaiveDateTime) with a statically known value.
+///
+/// The input is checked at compile time.
+///
+/// # Examples
+/// ```
+/// use chrono::datetime;
+///
+/// let _ = datetime!(2023-09-08 7:03);
+/// let _ = datetime!(2023-09-08 7:03:25);
+/// ```
+#[macro_export]
+macro_rules! datetime {
+    ($y:literal-$m:literal-$d:literal $h:literal:$min:literal:$s:literal) => {{
+        #[allow(clippy::zero_prefixed_literal)]
+        {
+            const DATE: $crate::NaiveDate = match $crate::NaiveDate::from_ymd_opt($y, $m, $d) {
+                Some(d) => d,
+                None => panic!("invalid calendar date"),
+            };
+            const SECS_NANOS: (u32, u32) = match $s {
+                60u32 => (59, 1_000_000_000),
+                s => (s, 0),
+            };
+            const TIME: $crate::NaiveTime =
+                match $crate::NaiveTime::from_hms_nano_opt($h, $min, SECS_NANOS.0, SECS_NANOS.1) {
+                    Some(t) => t,
+                    None => panic!("invalid time"),
+                };
+            DATE.and_time(TIME)
+        }
+    }};
+    ($y:literal-$m:literal-$d:literal $h:literal:$min:literal) => {{
+        #[allow(clippy::zero_prefixed_literal)]
+        {
+            const DATE: $crate::NaiveDate = match $crate::NaiveDate::from_ymd_opt($y, $m, $d) {
+                Some(d) => d,
+                None => panic!("invalid calendar date"),
+            };
+            const TIME: $crate::NaiveTime = match $crate::NaiveTime::from_hms_opt($h, $min, 0) {
+                Some(t) => t,
+                None => panic!("invalid time"),
+            };
+            DATE.and_time(TIME)
+        }
+    }};
+}
+
 #[cfg(test)]
 #[rustfmt::skip::macros(date)]
 mod tests {
-    use crate::{NaiveDate, NaiveTime};
+    use crate::{NaiveDate, NaiveDateTime, NaiveTime};
 
     #[test]
     fn init_macros() {
@@ -94,11 +142,25 @@ mod tests {
         assert_eq!(date!(2023-253), NaiveDate::from_yo_opt(2023, 253).unwrap());
         assert_eq!(time!(7:03), NaiveTime::from_hms_opt(7, 3, 0).unwrap());
         assert_eq!(time!(7:03:25), NaiveTime::from_hms_opt(7, 3, 25).unwrap());
+        assert_eq!(
+            time!(23:59:60),
+            NaiveTime::from_hms_nano_opt(23, 59, 59, 1_000_000_000).unwrap()
+        );
+        assert_eq!(
+            datetime!(2023-09-08 7:03),
+            NaiveDate::from_ymd_opt(2023, 9, 8).unwrap().and_hms_opt(7, 3, 0).unwrap(),
+        );
+        assert_eq!(
+            datetime!(2023-09-08 7:03:25),
+            NaiveDate::from_ymd_opt(2023, 9, 8).unwrap().and_hms_opt(7, 3, 25).unwrap(),
+        );
     }
 
     #[test]
     fn macros_are_const() {
         const DATE: NaiveDate = date!(2023-09-08);
         const TIME: NaiveTime = time!(7:03:25);
+        const NAIVEDATETIME: NaiveDateTime = datetime!(2023-09-08 7:03:25);
+        assert_eq!(DATE.and_time(TIME), NAIVEDATETIME);
     }
 }
