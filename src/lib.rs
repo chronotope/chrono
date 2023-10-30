dd3f2f81c4cf6f4d85723f122d7e637bd4bd674d
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-10-30 09:30:11 +0100

    Bump MSRV to 1.61

--- src/lib.rs
+++ src/lib.rs
@@ -359,7 +359,7 @@
 //!
 //! ## Rust version requirements
 //!
-//! The Minimum Supported Rust Version (MSRV) is currently **Rust 1.57.0**.
+//! The Minimum Supported Rust Version (MSRV) is currently **Rust 1.61.0**.
 //!
 //! The MSRV is explicitly tested in CI. It may be bumped in minor releases, but this is not done
 //! lightly.
