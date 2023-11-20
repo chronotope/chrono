621be2f932707fa31fead63cc11add8ac03e4365
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-11-20 09:38:30 +0100

    Improve code generation test

--- tests/win_bindings.rs
+++ tests/win_bindings.rs
@@ -1,5 +1,6 @@
 #![cfg(all(windows, feature = "clock", feature = "std"))]
 
+use similar_asserts::assert_eq;
 use std::fs;
 use windows_bindgen::bindgen;
 
@@ -7,7 +8,7 @@ use windows_bindgen::bindgen;
 fn gen_bindings() {
     let input = "src/offset/local/win_bindings.txt";
     let output = "src/offset/local/win_bindings.rs";
-    let existing = fs::read_to_string(output).unwrap();
+    let existing = fs::read_to_string(output).unwrap().replace("\r\n", "\n");
 
     let log = bindgen(["--etc", input]).unwrap();
     eprintln!("{}", log);
@@ -15,8 +16,6 @@ fn gen_bindings() {
     // Check the output is the same as before.
     // Depending on the git configuration the file may have been checked out with `\r\n` newlines or
     // with `\n`. Compare line-by-line to ignore this difference.
-    let new = fs::read_to_string(output).unwrap();
-    if !new.lines().eq(existing.lines()) {
-        panic!("generated file `{}` is changed.", output);
-    }
+    let new = fs::read_to_string(output).unwrap().replace("\r\n", "\n");
+    assert_eq!(new, existing);
 }
