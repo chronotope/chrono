afb9299318cd5bc1a060f75c0eb76cc9ed95b4b5
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-01-06 14:28:41 +0100

    Add Month::num_days()

--- src/month.rs
+++ src/month.rs
@@ -3,6 +3,7 @@ use core::fmt;
 #[cfg(any(feature = "rkyv", feature = "rkyv-16", feature = "rkyv-32", feature = "rkyv-64"))]
 use rkyv::{Archive, Deserialize, Serialize};
 
+use crate::naive::NaiveDate;
 use crate::OutOfRange;
 
 /// The month of the year.
@@ -160,6 +161,29 @@ impl Month {
             Month::November => "November",
             Month::December => "December",
         }
+    }
+
+    /// Get the length in days of the month
+    ///
+    /// Yields `None` if `year` is out of range for `NaiveDate`.
+    pub fn num_days(&self, year: i32) -> Option<u8> {
+        Some(match *self {
+            Month::January => 31,
+            Month::February => match NaiveDate::from_ymd_opt(year, 2, 1)?.leap_year() {
+                true => 29,
+                false => 28,
+            },
+            Month::March => 31,
+            Month::April => 30,
+            Month::May => 31,
+            Month::June => 30,
+            Month::July => 31,
+            Month::August => 31,
+            Month::September => 30,
+            Month::October => 31,
+            Month::November => 30,
+            Month::December => 31,
+        })
     }
 }
 
@@ -437,5 +461,12 @@ mod tests {
         let month = Month::January;
         let bytes = rkyv::to_bytes::<_, 1>(&month).unwrap();
         assert_eq!(rkyv::from_bytes::<Month>(&bytes).unwrap(), month);
+    }
+
+    #[test]
+    fn num_days() {
+        assert_eq!(Month::January.num_days(2020), Some(31));
+        assert_eq!(Month::February.num_days(2020), Some(29));
+        assert_eq!(Month::February.num_days(2019), Some(28));
     }
 }
