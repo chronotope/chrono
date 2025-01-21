bfdd4d74f0fd9a225d9e2d4d4dde89767efc816e
Steven Tang <sttang@blackberry.com>
2025-01-21 14:21:02 -0500

    Expose `format_into` and `format_into_io` for `DelayedFormat`
    Request for #1649
    - renamed format to format_into and made it public.
    - added format_into_io to allow format into a `std::io::Write`.
    - added unittests
    - added benchmarks for the 3 methods, `Display`, `format_into`, and
      `format_into_io`.

--- src/format/formatting.rs
+++ src/format/formatting.rs
@@ -97,7 +97,30 @@ impl<'a, I: Iterator<Item = B> + Clone, B: Borrow<Item<'a>>> DelayedFormat<I> {
         DelayedFormat { date, time, off: Some(name_and_diff), items, locale }
     }
 
-    fn format(&self, w: &mut impl Write) -> fmt::Result {
+    /// Formats `DelayedFormat` into an `std::io::Write` instance.
+    /// # Errors
+    /// This function returns an error if formatting into the `std::io::Write` instance fails.
+    #[cfg(feature = "std")]
+    pub fn format_into_io(self, w: &mut impl std::io::Write) -> fmt::Result {
+        // wrapper to allow reuse of the existing string based
+        // writers
+        struct IoWriter<W: std::io::Write> {
+            writer: W,
+        }
+        impl<W: std::io::Write> fmt::Write for IoWriter<W> {
+            #[inline]
+            fn write_str(&mut self, s: &str) -> fmt::Result {
+                self.writer.write_all(s.as_bytes()).map_err(|_| fmt::Error)
+            }
+        }
+        let mut writer = IoWriter { writer: w };
+        self.format_into(&mut writer)
+    }
+
+    /// Formats `DelayedFormat` into a `core::fmt::Write` instance.
+    /// # Errors
+    /// This function returns an error if formatting into the `core::fmt::Write` instance fails.
+    pub fn format_into(&self, w: &mut impl Write) -> fmt::Result {
         for item in self.items.clone() {
             match *item.borrow() {
                 Item::Literal(s) | Item::Space(s) => w.write_str(s),
@@ -321,7 +344,7 @@ impl<'a, I: Iterator<Item = B> + Clone, B: Borrow<Item<'a>>> DelayedFormat<I> {
 impl<'a, I: Iterator<Item = B> + Clone, B: Borrow<Item<'a>>> Display for DelayedFormat<I> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut result = String::new();
-        self.format(&mut result)?;
+        self.format_into(&mut result)?;
         f.pad(&result)
     }
 }
@@ -353,7 +376,7 @@ where
 
 /// Formats single formatting item
 #[cfg(feature = "alloc")]
-#[deprecated(since = "0.4.32", note = "Use DelayedFormat::fmt instead")]
+#[deprecated(since = "0.4.32", note = "Use DelayedFormat::fmt or DelayedFormat::format instead")]
 pub fn format_item(
     w: &mut fmt::Formatter,
     date: Option<&NaiveDate>,
@@ -610,6 +633,41 @@ mod tests {
     use crate::FixedOffset;
     #[cfg(feature = "alloc")]
     use crate::{NaiveDate, NaiveTime, TimeZone, Timelike, Utc};
+
+    #[cfg(all(feature = "std", feature = "alloc"))]
+    #[test]
+    fn test_delayed_format_into_io_matches_format_into() {
+        let dt = crate::DateTime::from_timestamp(1643723400, 123456789).unwrap();
+        let df = dt.format("%Y-%m-%d %H:%M:%S%.9f");
+
+        let mut dt_str = String::new();
+        let mut dt_vec_str = Vec::new();
+
+        df.format_into(&mut dt_str).unwrap();
+        df.format_into_io(&mut dt_vec_str).unwrap();
+
+        assert_eq!(dt_str, String::from_utf8(dt_vec_str).unwrap());
+        assert_eq!(dt_str, "2022-02-01 13:50:00.123456789");
+    }
+
+    #[cfg(all(feature = "std", feature = "unstable-locales", feature = "alloc"))]
+    #[test]
+    fn test_with_locale_delayed_format_into_io_matches_format_into() {
+        use crate::format::locales::Locale;
+        use crate::DateTime;
+
+        let dt = DateTime::from_timestamp(1643723400, 123456789).unwrap();
+        let df = dt.format_localized("%A, %B %d, %Y", Locale::ja_JP);
+
+        let mut dt_str = String::new();
+        let mut dt_vec_str = Vec::new();
+
+        df.format_into(&mut dt_str).unwrap();
+        df.format_into_io(&mut dt_vec_str).unwrap();
+
+        assert_eq!(dt_str, String::from_utf8(dt_vec_str).unwrap());
+        assert_eq!(dt_str, "火曜日, 2月 01, 2022");
+    }
 
     #[test]
     #[cfg(feature = "alloc")]
