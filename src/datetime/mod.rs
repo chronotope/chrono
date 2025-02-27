2115206bc3b0c520fe6bcc041aa724f3ad5286ea
Robert Bastian <me@robertbastian.dev>
2025-02-27 18:26:01 +0100

    Deprecate NaiveDateTime::UNIX_EPOCH

--- src/datetime/mod.rs
+++ src/datetime/mod.rs
@@ -858,7 +858,8 @@ impl DateTime<Utc> {
     }
 
     /// The Unix Epoch, 1970-01-01 00:00:00 UTC.
-    pub const UNIX_EPOCH: Self = Self { datetime: NaiveDateTime::UNIX_EPOCH, offset: Utc };
+    pub const UNIX_EPOCH: Self =
+        expect(NaiveDate::from_ymd_opt(1970, 1, 1), "").and_time(NaiveTime::MIN).and_utc();
 }
 
 impl Default for DateTime<Utc> {
