78c0fbcf181d3256c2c240a465feb8fd6c950451
Benjamin Brittain <ben@brittain.org>
2025-05-19 22:51:42 -0400

    Migrate to core::error::Error
    Stabilized in rust 1.81, behind a feature flag so
    as to not bump the current MSRV of 1.61.

--- src/month.rs
+++ src/month.rs
@@ -272,6 +272,9 @@ pub struct ParseMonthError {
 #[cfg(feature = "std")]
 impl std::error::Error for ParseMonthError {}
 
+#[cfg(all(not(feature = "std"), feature = "core-error"))]
+impl core::error::Error for ParseMonthError {}
+
 impl fmt::Display for ParseMonthError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "ParseMonthError {{ .. }}")
